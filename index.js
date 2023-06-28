756220d6f779ef6aa7c45ab19dcfde8ecc57e9c0
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-28 18:02:54 +0530

    setup layout in index.js and setup mongoose fot connect to database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const expressLayout = require('express-ejs-layouts');
+app.use(expressLayout);
+const db = require('./config/mongoose');
 
 
 app.use('/', require('./routes'));
