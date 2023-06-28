dbcf6f56024941047a3b9e1868f3554559d82dfe
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-06-28 18:50:21 +0530

    Setup all ejs page and remaining only task

--- controllers/homec_controller.js
+++ controllers/homec_controller.js
@@ -1,3 +1,17 @@
-module.exports.home = function(req, res){
-    return res.end("<h1>TODO LIST</h1>");
+//const db = require('./config/mongoose')
+const TodoLists = require('../models/todo_list')
+// function for redirecting to main home page
+module.exports.home = async function(req,res){
+    // fetching using mongoose
+   try {
+    let todo = await TodoLists.find({});
+        
+        return res.render('home',{
+            title:"Home",
+            todoList:todo
+          });
+   } catch (error) {
+    console.log(error,'Error is getting while fatching data');
+    return;
+   }
 }
--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const db = require('./config/mongoose');
 
 
 app.use('/', require('./routes'));
+app.use(express.static('./assets')) // for getting static
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true)
 
-
+app.set('view engine','ejs');
+app.set('views','./views')
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
